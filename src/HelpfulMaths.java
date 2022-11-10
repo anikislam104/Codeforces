0dcc317e67dc311ead9ed0a4122dd72636ddc4f5
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-11-10 17:34:05 +0600

    339A - Helpful Maths

--- src/HelpfulMaths.java
+++ src/HelpfulMaths.java
@@ -14,15 +14,30 @@ public class HelpfulMaths {
             int idx=0;
             for(int i=0;i<len;i++){
                 if(s1.charAt(i)!='+'){
-                    numbers[idx]=s1.charAt(i);
+                    numbers[idx]=s1.charAt(i)-48;
                     idx++;
                 }
             }
+
             for(int i=0;i<arr_len;i++){
                 int min=4;
                 int index=i;
                 for(int j=i;j<arr_len;j++){
-
+                    if(numbers[j]<min){
+                        min=numbers[j];
+                        index=j;
+                    }
+                }
+                int temp=numbers[i];
+                numbers[i]=numbers[index];
+                numbers[index]=temp;
+            }
+            for(int i=0;i<arr_len;i++){
+                if(i!=arr_len-1){
+                    System.out.print(numbers[i]+"+");
+                }
+                else {
+                    System.out.print(numbers[i]);
                 }
             }
         }
